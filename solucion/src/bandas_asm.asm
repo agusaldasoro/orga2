509e3a100ac637bec2dc65cc80ce8212d8fb30ce
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2014-09-14 02:49:25 -0300

    bandas' incomplete implementation.

--- solucion/src/bandas_asm.asm
+++ solucion/src/bandas_asm.asm
@@ -9,7 +9,14 @@ color3 db 128,128,128,255
 color4 db 192,192,192,255
 color5 db 255,255,255,255
 
+color1_bound dw 96,96,96,96,96,96,96,96
+color2_bound dw 288,288,288,288,288,288,288,288
+color3_bound dw 480,480,480,480,480,480,480,480
+color4_bound dw 672,672,672,672,672,672,672,672
+
 rgb_only_mask dd 0x00FFFFFF,0x00FFFFFF,0x00FFFFFF,0x00FFFFFF
+
+all_1_mask    dd 0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF
 
 section .text
 ;void bandas_asm    (
@@ -49,24 +56,40 @@ bandas_asm:
     movdqu xmm2,xmm1
 
     ; Pongo ceros en alpha así no jode en la suma 
-    ; (podría aumentar en 255 los valores con los que comparo)
+    ; (también podría aumentar en 255 los valores con los que comparo)
     movdqu xmm15,[rgb_only_mask]
     pand xmm1,xmm15
     pand xmm2,xmm15
     
     pxor xmm7,xmm7
+    pxor xmm8,xmm8
     punpcklbw xmm1,xmm7 ; baja
     punpckhbw xmm2,xmm7 ; alta
 
 
-    phaddw xmm1,xmm1
-    phaddw xmm1,xmm1
-    phaddw xmm2,xmm2
-    phaddw xmm2,xmm2
+    pxor xmm7,xmm7
+    phaddw xmm1,xmm7
+    phaddw xmm7,xmm1
+    
+    movdqu xmm1,xmm7
+
+    pxor xmm8,xmm8
+
+    phaddw xmm2,xmm8
+    phaddw xmm2,xmm8
+
+    paddw xmm1,xmm2
+
+    movdqu xmm15,[color1_bound]
+    movdqu xmm0,xmm1
+    pcmpgtw xmm1,xmm15 ; pone ceros en dst donde no se cumpla la condición (greater than)
+    ; ahora tengo 0 en las posiciones donde tiene que ir color1
+    movdqu xmm15,[all_1_mask]
+    pandn xmm1,xmm15 ; !xmm1
+    
+
 
     ; if (i < 96) {
-    pcmpgtd xmm1,xmm1 ; si un word de xmm0 es menor al word de xmm1 => 0
-
     ;   return 0;
     ; } else if (i >= 96 && i < 288) {
     ;   return 64;
@@ -77,12 +100,12 @@ bandas_asm:
     ; }
     ; return 255;
 
-    movdqu xmm0,[rdi+rax] ; agarró 16 bytes del source!
-    movdqu xmm1,xmm0      ; y me hago una copia
+    ; movdqu xmm0,[rdi+rax] ; agarró 16 bytes del source!
+    ; movdqu xmm1,xmm0      ; y me hago una copia
 
-    pxor xmm2,xmm2
+    pxor xmm7,xmm7        ; limpiamos xmm2
 
-    movdqa [rsi+rax],xmm6
+    movdqa [rsi+rax],xmm0 ; 
 
 
     add r15,4 ; avanzo 4 sobre x
