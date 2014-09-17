361ccb8010f25f860cbeed753055b718349c2674
Ignacio Mariano Tirabasso <itirabasso@ws11.labo5>
2014-09-16 21:07:59 -0300

    Implentacion de cropflip en C

--- solucion/src/cropflip_c.c
+++ solucion/src/cropflip_c.c
@@ -16,6 +16,27 @@ void cropflip_c    (
 {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+	
+	int x, y, x2, y2;
 
+	y2 = tamy-1;
+	for (y = offsety; y < offsety+tamy	; y++) {
+		x2 = tamx;
+		for (x = offsetx; x < offsetx+tamx; x++) {
+						
+			unsigned char r = src_matrix[y][x*4+0];
+			
+			unsigned char g = src_matrix[y][x*4+1];
+			unsigned char b = src_matrix[y][x*4+2];
+			unsigned char a = src_matrix[y][x*4+3];
+			dst_matrix[y2][x2*4-4] = r;
+			dst_matrix[y2][x2*4-3] = g;
+			dst_matrix[y2][x2*4-2] = b;
+			dst_matrix[y2][x2*4-1] = a;
+			x2--;
+			//printf("HOLA\n");
+		}
+		y2--;
+	} 
 
 }
