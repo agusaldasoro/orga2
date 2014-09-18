bb9d721ceb0345f892f032c6d3f1da475d3e9e3b
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2014-09-17 21:46:02 -0300

    Cropflip andando!

--- solucion/src/cropflip_asm.asm
+++ solucion/src/cropflip_asm.asm
@@ -13,9 +13,8 @@ section .text
 cropflip_asm:
 	push rbp
 	mov rbp,rsp
-	
-	push rdi
-	push rsi
+	push rbx
+	push rcx
 	push r12
 	push r13
 	push r14
@@ -40,94 +39,50 @@ cropflip_asm:
 	mov r10,r15 ; y
 	mov r11,r14 ; x
 
-	dec r13    ; y2 = tamy-1;
-	add r13,r15 ; offsety+tamy
+	mov rcx,r13 ; y2 = tamy
+	dec rcx     ; y2 = tamy-1;
+
+	add r13,r15 ; r13 = offsety+tamy
+	
 	mov rbx,r12
-	add rbx,r14 ; offsetx+tamx
+	add rbx,r14 ; rbx = offsetx+tamx
 .loop_y:
-	; cmp r10,offsety+tamy
-	cmp r10,r13 ; offsety+tamy
+	cmp r10,r13 ; y < offsety+tamy
 	jge .endloop_y
-	mov r11,r14 
+	mov r11,r14
+	mov rdx,r12 
 .loop_x:
-	; mov rbx,r14
-	; add rbx,r12
-	cmp r11,rbx ; offsetx+tamx
+	cmp r11,rbx ; x < offsetx+tamx
 	jge .endloop_x
 
 	xor rax,rax
+
 	mov rax,r10
-	mul r8
-	lea rax,[rax+r11*4]
+	imul rax,r8
+	lea rax,[rax+r11*4] 
 	movdqu xmm0,[rdi+rax]
 
-	;mov rax,r15
-	;mul r8
-	;add rax,rdi
-	; lea rax,[rax+r11*4] ; si maxi, ya sé
+	pshufd xmm0, xmm0,0x1B ; 00 01 10 11
 
-	; pshufd xmm0, xmm0,0xE4
-
-	mov rax,r
-	mul r9
-	lea rax,[rax+r11*4]
-	movdqu [rsi+rax],xmm0
+	mov rax,rcx
+	imul rax,r9
+	lea rax,[rax+rdx*4]
+	movdqu [rsi+rax-16],xmm0
 
 	add r11,4
+	sub rdx,4
 	jmp .loop_x
 .endloop_x:
 	inc r10
+	dec rcx
 	jmp .loop_y
 .endloop_y:
 	
-	; mov R10, RCX
-	; mov R14D,R14D
-	; LEA RAX,[RDI+R14*4]
-
-	; mov r8d,r8d
-	; mov r15d,r15d
-	; dec r15
-	; imul r15, r8
-	; LEA RAX, [RAX+R15]
-
-	; mov R11,RSI
-	; mov R9D,R9D
-
-	; mov R13D, R13D
-	; mov RCX, R13
-
-	; mov R12D, R12D
-	; .CICLO:
-	; mov R9, RCX
-	; mov RCX, R12
-	; SAR ecx,2
-	; dec ecx
-	; .CICLO2:
-	; movDQu XMM0, [RAX]
-	; PSHUFD XMM0, XMM0,0xE4
-	; movDQu [R11], XMM0
-
-
-	; LEA RAX, [RAX+16]
-	; ; LEA R11, [R11+16]
-	; add r11,16															
-	; LOOP .CICLO2
-
-	; LEA  RAX, [RAX+R8]
-	; LEA R11,[R11+R9]
-	; mov RCX, R9
-	; LOOP .CICLO
-
-
-
-	
-
-
-	pop R15
-	pop R14
-	pop R13
-	pop R12
-	pop rsi
-	pop rdi
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rcx
+	pop rbx
 	pop rbp
     ret
