0ab3f8b54ddfff583891941a809161d538414909
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2014-09-19 03:40:15 -0300

    Motion blur en C.

--- solucion/src/mblur_c.c
+++ solucion/src/mblur_c.c
@@ -21,9 +21,31 @@ void mblur_c    (
     {
         for (int j = 0; j < cols; j++)
         {
-            bgra_t *p_d = 0;
-            bgra_t *p_s = 0;
-            *p_d = *p_s;
+            if (i < 2 || j < 2) {
+                dst_matrix[i][j*4+0] = 0;
+                dst_matrix[i][j*4+1] = 0;
+                dst_matrix[i][j*4+2] = 0;
+                dst_matrix[i][j*4+3] = 0;
+                continue;
+            }
+
+            bgra_t *a = &src_matrix[i][j*4];
+            bgra_t *c = &src_matrix[i+1][(j+1)*4];
+            bgra_t *b = &src_matrix[i-1][(j-1)*4];
+            bgra_t *d = &src_matrix[i+2][(j+2)*4];
+            bgra_t *e = &src_matrix[i-2][(j+2)*4];
+
+            bgra_t dst;
+
+            dst.r = 0.2 * (a->r + b->r + c->r + d->r + e->r);
+            dst.g = 0.2 * (a->g + b->g + c->g + d->g + e->g);
+            dst.b = 0.2 * (a->b + b->b + c->b + d->b + e->b);
+            dst.a = 0.2 * (a->a + b->a + c->a + d->a + e->a);
+
+            dst_matrix[i][j*4+0] = dst.b;
+            dst_matrix[i][j*4+1] = dst.g;
+            dst_matrix[i][j*4+2] = dst.r;
+            dst_matrix[i][j*4+3] = dst.a;
         }
     }
  
