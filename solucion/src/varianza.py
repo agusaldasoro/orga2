3106dd6d70902776d8725e816883fff2cc7da4c3
Aldasoro Agustina <aaldasoro@ws18.labo7>
2014-09-26 15:48:16 -0300

    En varianza puse varianza, y cree un archivo esperanza

--- solucion/src/varianza.py
+++ solucion/src/varianza.py
@@ -10,19 +10,37 @@ def suma(arr):
 
     return ret
 
+def restarEspe(arr, esp):
+    for i in range(len(arr)):
+        arr[i]  = float(arr[i]) - esp
+	arr[i] *= int(arr[i])
+    return arr
+
 for i in range(4):
     filtro = lines[0]
     print filtro[:len(filtro)-1]
 
     ret = suma(lines[1:10])
+    esp = ret/float(10)
+    restarEspe(lines[1:10], esp)
+    ret = suma(lines[1:10])
     print "10 pasadas: " + str(ret/float(10))
 
     ret = suma(lines[11:110])
+    esp = ret/float(100)
+    restarEspe(lines[11:110], esp)
+    ret = suma(lines[11:110])
     print "100 pasadas: " + str(ret/float(100))
 
+    ret = suma(lines[111:310])
+    esp = ret/float(200)
+    restarEspe(lines[111:310], esp)
     ret = suma(lines[111:310]);
     print "200 pasadas: " + str(ret/float(200))
 
+    ret = suma(lines[311:1310])
+    esp = ret/float(1000)
+    restarEspe(lines[311:1310], esp)
     ret = suma(lines[311:1310])
     print "1000 pasadas: " + str(ret/float(1000))
 
