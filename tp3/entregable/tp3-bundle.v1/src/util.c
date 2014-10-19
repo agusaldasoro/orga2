34284c6316edd8481a2f2e73686f852b4120e405
= <ignacio.tirabasso@gmail.com>
2014-10-19 16:49:20 -0300

    printf, strlen, digits, putc, print_int.

---
+++ tp3/entregable/tp3-bundle.v1/src/util.c
@@ -0,0 +1,115 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+  definicion de funciones del scheduler
+*/
+
+#include "util.h"
+#include "screen.h"
+#include <stdarg.h>
+
+unsigned int strlen(const char *s) {
+    unsigned int len = 0;
+
+    while(*s != 0) {
+        len++;
+        s++;
+    }
+    return len;
+}
+
+void putc(char c, unsigned int col, unsigned int row) {
+    // avoid black print bug
+    unsigned char attr = getFormat(C_FG_WHITE, 0, C_BG_BLACK, 0);
+
+    ca (*p)[VIDEO_COLS] = (ca (*)[VIDEO_COLS]) VIDEO;
+    p[row][col].c = c;
+    p[row][col].a = attr;
+
+}
+
+int digits(unsigned int n) {
+    int digits = 0;
+    while(n >= 10) {
+        n = n / 10;
+        digits++;
+    }
+
+    return digits + 1;
+} 
+
+void print_int(int n, unsigned int col, unsigned int row) {
+    char numbers[10] = "0123456789";
+    int d = digits(n);
+    while(n > 0) {
+        putc(numbers[n % 10], col+d-1, row);
+        n /= 10;
+        d--;
+    }
+}
+
+// Solo voy a imprimir ints y strings.
+void printf(unsigned int col, unsigned int row, const char *format, ...) {
+    // unsigned char attr = getFormat(C_FG_WHITE, 0, C_BG_BLACK, 0);
+    
+    va_list args;
+    va_start(args, format);
+
+    col--;
+    int nextArg = 0;
+    int n;
+    // unsigned int* p;
+    char *s;
+    while(*format != 0) {
+        if (nextArg == 0) {
+            if (*format == '%') {
+                nextArg = 1;
+                format++;
+                continue;
+            }
+            putc(*format, col, row);
+            col++;
+        } else {
+            switch(*format) {
+                case 'd':
+                    n = va_arg(args,int);
+                    print_int(n, col, row);
+                    col += digits(n);
+                    break;
+                case 'h':
+                    n = va_arg(args,int);
+                    print_string("0x", col, row, 0);
+                    print_hex(n, 8, col+2, row, 0);
+                    col += digits(n) + 2;
+                    break;
+                case 'p':
+                    n = va_arg(args, unsigned int);
+                    print_string("0x",col, row,0);
+                    print_hex(n, 8, col+2, row, 0);
+                    col += digits(n) + 2;
+                    break;
+                case 's':
+                    // check null pointer and print "(null)"
+                    s = va_arg(args, char*);
+                    print_string(s, col, row, 0);
+                    col += strlen(s);
+                    break;
+                default:
+                    print_string("Unknown format!", col, row, 0);
+                    // print_string("Unknown format! Stay calm, it's just a bug. AAAAAAA WE ALL GONNA DIE!!!", row, col, 0);
+            }
+        }
+        
+        // use defines!
+        if (col == 80) {
+            col = 0;
+            row++;
+        }
+        nextArg = 0;
+        format++;
+    }
+    va_end(args);
+
+
+}
---
+++ tp3/entregable/tp3-bundle.v1/src/util.h
@@ -0,0 +1,14 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+// #include "defines.h"
+
+int digits(unsigned int n);
+
+unsigned int strlen(const char *s);
+void printf(unsigned int col, unsigned int row, const char *format, ...);
+void putc(char c, unsigned int col, unsigned int row);
+void print_int(int number, unsigned int col, unsigned int row);
+
+
+#endif  /* !__UTIL_H__ */
