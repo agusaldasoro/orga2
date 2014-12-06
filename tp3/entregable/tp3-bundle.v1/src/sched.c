48f6a79d010afa00a603a111af91e42d412c35b1
MaximilianoRey <rey.maximiliano@gmail.com>
2014-12-06 17:29:04 -0300

    el que falto

--- tp3/entregable/tp3-bundle.v1/src/sched.c
+++ tp3/entregable/tp3-bundle.v1/src/sched.c
@@ -6,12 +6,16 @@
 */
 
 #include "sched.h"
+  #include "i386.h"
 
 unsigned short sched_proximo_indice() {
   return 0;
 }
 
 void start_zombie(u8 player, u8 class, u8 y) {
+	breakpoint();
 	page_directory* pd = mmu_inicializar_dir_zombie(player, class, y);
-	init_tss(get_free_tss(player), (u32) pd, ZOMBIE_VIRTUAL, ZOMBIE_VIRTUAL + PAGE_SIZE, 0x48, 0x58, 0x202);
+	breakpoint();
+	init_tss(get_free_tss(player), (u32) pd, ZOMBIE_VIRTUAL, ZOMBIE_VIRTUAL + PAGE_SIZE, (0x48| 3), (0x58 | 3), 0x202);
+	breakpoint();
 }
